4a121a7f5078183dfabfc4604e9645752566d7d1
Fayiz kv <fayizkvcklm2002@gmail.com>
2025-01-20 11:19:11 +0530

    pages setup done, firebase integrated, api for fetching questions implemetned

--- src/commponents/Quizmaker.js
+++ src/commponents/Quizmaker.js
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
+import React, { useDebugValue, useState } from 'react';
 import axios from 'axios';
 
 function Quizmaker(){
 
     const [question, setQuestion] = useState('');
     const [answer, setAnswer] = useState('');
+    const [category, setCategory] = useState('');
+    const [difficulty, setDifficulty] = useState('');
 
     function handleSubmit(){ 
     console.log(question, answer);  
     }
     function generateQuesions(){
-        axios.get('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple').then((response)=>
+        axios.get(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=multiple`).then((response)=>
         console.log(response.data.results))
     }
 
@@ -25,6 +27,30 @@ function Quizmaker(){
             </label>
                 <button onClick={handleSubmit}> Submit </button>
                 <label> Auto Generate Questions </label>
+                <div>
+        <label>Difficulty: </label>
+        <select
+          value={difficulty ? difficulty : "easy"}
+          onChange={(e) => setDifficulty(e.target.value)}
+        >
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+        </select>
+      </div>
+      
+      <div>
+        <label>Category: </label>
+        <select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="9">General Knowledge</option>
+          <option value="21">Sports</option>
+          <option value="18">Science: Computers</option>
+          {/* Add more categories as needed */}
+        </select>
+      </div>
             
             <button onClick={generateQuesions}> Auto Generate </button>
         </div>
